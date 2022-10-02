ef54c8d32303fb9fd9d75a4496817738903f4863
Dixa Patel <87237633+DxPtl01@users.noreply.github.com>
2022-10-01 20:55:08 -0400

    Update GridColors.java

--- Maze/GridColors.java
+++ Maze/GridColors.java
@@ -1,9 +1,6 @@
 package Maze;
 /*
- *  Author:Dixaben Patel 10456998
- *  CS 570A - Data Structure
- *  Homework Assignment #3 The Maze
- * 
+ *  Author:Dixa Patel 
  */
 import java.util.*;
 import java.awt.Color;
